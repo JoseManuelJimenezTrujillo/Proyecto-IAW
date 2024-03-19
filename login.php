e3602ac93658571304fd61d815b160bffa52be2c
Jose <jjimtru229a@ieslacampina.es>
2024-03-19 13:56:07 +0100

    Creación del login

---
+++ login.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+
+	<link rel="stylesheet" href="estilos.css">
+
+
+	<!-- Optional JavaScript -->
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+	<script src="js/jquery-3.4.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.dataTables.min.js"></script>
+
+	<title>Crazy-Drivers</title>
+
+
+
+</head>
+<body>
+<?php
+require 'conexion.php';
+$usuario = $_POST['usuario'];
+$contraseña = $_POST['contraseña'];
+
+//se prepara y ejecuta la sentencia
+$sql = "SELECT * FROM usuarios where usuario='$usuario' and contraseña='$contraseña' LIMIT 1";
+$resultado = $mysqli->query($sql);
+
+//se extrae el registro. no se hace en bucle porque el resultado debe ser unico
+$fila = $resultado->fetch_assoc();
+if ($fila){
+    if ($usuario=="admin"){
+    header("location: panel_de_control.php");
+    } else{
+        header("location:inicio.php");
+    }
+}else{
+    echo "<div class='d-flex justify-content-center justify-content-sm-center justify-content-md-center justify-content-lg-center justify-content-xl-center' style='width:100%'>";
+    echo "<h3>Usuario o contraseña incorrectos.</h3>";
+    echo "</div>";
+    echo "<div class='d-flex justify-content-center justify-content-sm-center justify-content-md-center justify-content-lg-center justify-content-xl-center' style='width:100%'>";
+    echo "<p><p><a href='index.php' class='btn btn-primary'>Volver</a></p>";
+    echo "</div>";
+} 
+
+?>
+</body>
+</html>
