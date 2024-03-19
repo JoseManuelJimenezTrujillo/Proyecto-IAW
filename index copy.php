969b553f2a25394459503f2ab588a67b24074f36
Jose <jjimtru229a@ieslacampina.es>
2024-03-19 13:49:31 +0100

    Instalación de boostrap

---
+++ index copy.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/jquery.dataTables.min.css">
+	<link rel="stylesheet" href="login.css">
+    <title>Crazy-Drivers</title>
+</head>
+<body>
+<div class="login">
+	<div class="container">
+		<div class="logo">Crazy-Drivers</div>
+		<form class="form" method="POST" action="login.php">
+			<input type="usuario" class="input" placeholder="Usuario" name="usuario" required>
+			<input type="password" class="input" placeholder="Contraseña" name="contraseña" required>
+			<button type="submit" class="button">Iniciar sesión</button>
+		</form>
+		
+        </div>
+	</div>
+</body>
+</html>
